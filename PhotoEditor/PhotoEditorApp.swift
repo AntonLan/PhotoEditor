bf5699ea79aca24fb1e6feeab15e9b914547c2a6
AntonLan <lanqel@gmail.com>
2024-04-27 15:48:19 +0200

    Initial Commit

---
+++ PhotoEditor/PhotoEditorApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditorApp.swift
+//  PhotoEditor
+//
+//  Created by Anton Gerasimov on 27.04.2024.
+//
+
+import SwiftUI
+
+@main
+struct PhotoEditorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
